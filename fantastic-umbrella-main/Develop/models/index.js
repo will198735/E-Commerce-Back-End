84e600ff997a53d3315bf1cab8a8828d304425d5
will198735 <recinoswilfredo@gmail.com>
2023-03-09 23:25:33 -0800

    on model set up index.js

--- fantastic-umbrella-main/Develop/models/index.js
+++ fantastic-umbrella-main/Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'productTag_tag',
+  foreignKey: 'Product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'productTag_product',
+  foreignKey: 'Tag_id',
+});
 
 module.exports = {
   Product,
